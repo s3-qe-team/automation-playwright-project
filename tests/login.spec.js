67e0f10f68139c50fcdede219bc6a94b4f4702d5
LehangQc <lehang.qcengineer@gmail.com>
2026-01-28 17:09:50 +0700

    Update more test steps for Test Case 2

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,6 +9,9 @@
   let loginPage;
   const email = "automationtest01@gmail.com";
   const password = "Abc123456";
+  const cssValue = "color";
+  const expectedColorInRba = "rgb(255, 165, 0";
+  const loginPageTitle = "Login to your account";
   
 test.beforeEach(async ({ page }) => {
   const basePage = new BasePage(page);
@@ -17,8 +20,10 @@ test.beforeEach(async ({ page }) => {
 
  test('Test Case 2: Login User with correct email and password', async ({ page }) => {
   const homePage = new HomePage(page);
+  await expect(homePage.homeButton).toHaveCSS(cssValue, expectedColorInRba);
   loginPage = await homePage.openLoginPage();
   await expect(page).toHaveURL('/login');
+  await expect(loginPage.loginPageTitle).toHaveText(loginPageTitle);
   await loginPage.enterTextToEmailTextBox(email);
   await loginPage.enterTextToPasswordTextBox(password);
   await loginPage.clickToLoginButton();
