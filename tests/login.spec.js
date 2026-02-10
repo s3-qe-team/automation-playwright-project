c9dc547c7c95bfd4b9d29f13a92cfdeaf717c853
Quyen Tran <quyntrn77@gmail.com>
2026-02-10 13:13:17 +0700

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,6 +4,8 @@ import BasePage from '../pages/base.page.js';
 
 const email = "automationtest01@gmail.com";
 const password = "Abc123456";
+const cssValue = "color";
+const expectedColorInRba = "rgb(255, 165, 0)";
 const expectedLoginPageTitle = "Login to your account";
 
 test.describe('@smoke', () => {
@@ -14,14 +16,14 @@ test.describe('@smoke', () => {
 
   test('Test Case 2: Login User with correct email and password', async ({ page }) => {
     const homePage = new HomePage(page);
-    await homePage.verifyHomePageIsVisible();
-    const loginSignupPage = await homePage.openLoginSignupPage();
+    await expect(homePage.homeButton).toHaveCSS(cssValue,expectedColorInRba);
+    const loginPage = await homePage.openLoginPage();
     await expect(page).toHaveURL('/login');
-    await expect(loginSignupPage.loginPageTitle).toBeVisible();
-    await expect(loginSignupPage.loginPageTitle).toHaveText(expectedLoginPageTitle);
-    await loginSignupPage.enterTextToEmailTextBox(email);
-    await loginSignupPage.enterTextToPasswordTextBox(password);
-    await loginSignupPage.clickToLoginButton();
-    await expect(loginSignupPage.loginSuccessText).toBeVisible();
+    await expect(loginPage.loginPageTitle).toBeVisible();
+    await expect(loginPage.loginPageTitle).toHaveText(expectedLoginPageTitle);
+    await loginPage.enterTextToEmailTextBox(email);
+    await loginPage.enterTextToPasswordTextBox(password);
+    await loginPage.clickToLoginButton();
+    await expect(loginPage.loginSuccessText).toBeVisible();
   });
 })
